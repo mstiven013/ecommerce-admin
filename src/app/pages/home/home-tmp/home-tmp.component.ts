d82530c0939dc5025ed1494a39c5ba6ec94d4820
Stiven Lopez <32859798+mstiven013@users.noreply.github.com>
2018-09-10 10:27:04 -0500

    Add jquery, icomoon, select2 styles & scripts + Recover password send email form

--- src/app/pages/home/home-tmp/home-tmp.component.ts
+++ src/app/pages/home/home-tmp/home-tmp.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-tmp',
-  templateUrl: './home-tmp.component.html'
+  templateUrl: './home-tmp.component.html',
+  styleUrls: ['./home-tmp.component.scss']
 })
 export class HomeTmpComponent implements OnInit {
 
--- src/app/pages/home/recover-password/recover-password-page.component.ts
+++ src/app/pages/home/recover-password/recover-password-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth/auth.service';
+import { MapErrorService } from '../../../services/error/map-error.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recover-password-page',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecoverPasswordPageComponent implements OnInit {
 
-  constructor() { }
+  recoverform: FormGroup;
+  error = { show: false, msg: '' }
+  loader = { show: false, position: 'absolute', align: 'top', mode: "indeterminate" }
+  showForm: Boolean = true;
+
+  constructor(
+    private _authService: AuthService,
+    private _mapError: MapErrorService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+
+    //Generate form
+    this.recoverform = new FormGroup({
+      'email': new FormControl(null, [ Validators.required, Validators.email ])
+    });
+  }
+
+  sendEmail(frm) {
+    this.loader.show = true;
+    console.log(frm.value)
+    this.loader.show = false;
   }
 
 }
